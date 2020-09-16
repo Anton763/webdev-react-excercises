a9e42640de084b43133ddb04a1728e30c416eaab
Anton763 <54988300+Anton763@users.noreply.github.com>
2020-09-16 14:24:24 +0300

    completed 1 exercise

---
+++ Task1/src/index.js
@@ -0,0 +1,6 @@
+console.log("hello");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/like.js";
+
+ReactDOM.render(<App />, document.getElementById("root"));
